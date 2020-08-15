ac421066b02fda2aab878dd39d7ee3fa22657f34
Mac <mazharulislame.cse@gmail.com>
2020-08-15 22:42:01 +0600

    Search Field Implemented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,16 +13,47 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         centerTitle: true,
         leading: IconButton(
-            icon: Icon(Icons.account_circle), iconSize: 30.0, onPressed: (){},),
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
         title: Text('Food Delivery'),
         actions: <Widget>[
           FlatButton(
-            onPressed: (){},
-            child: Text('Cart (${currentUser.cart.length})', style: TextStyle(
-              color: Colors.white,
-              fontSize: 20.0,
-            ),),
+            onPressed: () {},
+            child: Text(
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
           )
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 16.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8,color: Theme.of(context).primaryColor),
+                ),
+                hintText: 'Search Food or Restaurants',
+                prefixIcon: Icon(Icons.search,size: 30.0,),
+                suffixIcon: IconButton(icon: Icon(Icons.clear,size: 30.0,), onPressed: (){})
+              ),
+            ),
+          ),
         ],
       ),
     );
